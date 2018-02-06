0857ede3815f601bbc198d2092aa4d4bcccbc079
MagicTrevor <MagicTrevor@icloud.com>
2018-02-05 23:59:01 -0700

    add 'in' to T to make it contravariant

--- src/Harmony.Core/Models/IEventHandler.cs
+++ src/Harmony.Core/Models/IEventHandler.cs
@@ -1,6 +1,6 @@
 namespace Harmony.Core.Models
 {
-    public interface IEventHandler<T> where T : IEvent
+    public interface IEventHandler<in T> where T : IEvent
     {
         void Handle(T @event);
     }
